9df975e833c4ec78b89cb46eb6632d747fb68527
Mateusz Surjak <surjak1999@gmail.com>
2021-03-14 09:40:23 +0100

    initial commit

---
+++ src/main/java/com/image/manager/edgeserver/EdgeserverApplication.java
@@ -0,0 +1,13 @@
+package com.image.manager.edgeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdgeserverApplication.class, args);
+    }
+
+}
