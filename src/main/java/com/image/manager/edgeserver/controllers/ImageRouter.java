07ef347482650574a64f65c6a7ff5ab6cb639d01
nazkord <nazkord@gmail.com>
2021-04-03 11:42:07 +0200

    feat: create test route

--- src/main/java/com/image/manager/edgeserver/controllers/ImageRouter.java
+++ src/main/java/com/image/manager/edgeserver/controllers/ImageRouter.java
@@ -1,6 +1,5 @@
 package com.image.manager.edgeserver.controllers;
 
-import com.image.manager.edgeserver.BufferedImageConverter;
 import com.image.manager.edgeserver.OriginFacade;
 import com.image.manager.edgeserver.model.Operation;
 import com.image.manager.edgeserver.model.OperationFactory;
@@ -10,25 +9,22 @@ import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
-import java.awt.image.BufferedImage;
 import java.util.List;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 /**
  * Created by surjak on 25.03.2021
  */
 @Configuration
-public class RouterConfiguration {
+public class ImageRouter {
 
     private final OriginFacade originFacade;
     private final OperationFactory operationFactory;
 
-    public RouterConfiguration(OriginFacade originFacade, OperationFactory operationFactory) {
+    public ImageRouter(OriginFacade originFacade, OperationFactory operationFactory) {
         this.originFacade = originFacade;
         this.operationFactory = operationFactory;
     }
@@ -41,7 +37,7 @@ public class RouterConfiguration {
      * http://localhost:8080/small-image.png?op=scale&w=700&h=700&op=crop&w=50&h=50&x=50&y=50 combo :)
      */
     @Bean
-    public RouterFunction<ServerResponse> route() {
+    public RouterFunction<ServerResponse> imageRoutes() {
         return RouterFunctions
                 .route(GET("/{fileName}"),
                         serverRequest -> {
