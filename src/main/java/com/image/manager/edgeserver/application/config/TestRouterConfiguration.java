90f9aca258ab65fd0489b97953974e602f707b7d
nazkord <nazkord@gmail.com>
2021-04-08 00:15:20 +0200

    added missing annotation

--- src/main/java/com/image/manager/edgeserver/application/config/TestRouterConfiguration.java
+++ src/main/java/com/image/manager/edgeserver/application/config/TestRouterConfiguration.java
@@ -1,6 +1,7 @@
 package com.image.manager.edgeserver.application.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
@@ -9,6 +10,7 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
+@Configuration
 public class TestRouterConfiguration {
 
     @Bean
