203cc42f0ed595a7193ccb0929736dc861cbd553
Mateusz Surjak <surjak1999@gmail.com>
2021-04-08 18:17:18 +0200

    simple caching

--- src/main/java/com/image/manager/edgeserver/domain/origin/OriginFacade.java
+++ src/main/java/com/image/manager/edgeserver/domain/origin/OriginFacade.java
@@ -1,5 +1,7 @@
 package com.image.manager.edgeserver.domain.origin;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import com.image.manager.edgeserver.common.converter.BufferedImageConverter;
 import com.image.manager.edgeserver.domain.operation.Operation;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,8 +10,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 import java.awt.image.BufferedImage;
+import java.time.Duration;
 import java.util.List;
 
 
@@ -21,11 +25,19 @@ public class OriginFacade {
 
     @Value("${origin.host}")
     private String originUrl;
+    private final WebClient webClient;
+    private final Cache<String, byte[]> requestCache;
 
     private final BufferedImageConverter imageConverter;
 
     public OriginFacade(BufferedImageConverter imageConverter) {
+        webClient = WebClient.create();
         this.imageConverter = imageConverter;
+        this.requestCache = Caffeine.newBuilder()
+                .maximumSize(1000)
+                .expireAfterWrite(Duration.ofMinutes(10))
+                .build();
+
     }
 
     public Mono<byte[]> getImageAndApplyOperations(String fileName, List<Operation> operations) {
@@ -35,12 +47,18 @@ public class OriginFacade {
     }
 
     public Mono<byte[]> fetchImageFromOrigin(String imageName) {
-        return WebClient.create()
+        byte[] bytes = requestCache.getIfPresent(imageName);
+        if (bytes != null) {
+            return Mono.just(bytes);
+        }
+        return webClient
                 .get()
                 .uri(originUrl + "/" + imageName)
                 .accept(MediaType.IMAGE_JPEG, MediaType.IMAGE_PNG)
                 .retrieve()
-                .bodyToMono(byte[].class);
+                .bodyToMono(byte[].class)
+                .subscribeOn(Schedulers.boundedElastic())
+                .doOnSuccess(b -> requestCache.put(imageName, b));
     }
 
     private Mono<BufferedImage> applyOperationsOnImage(List<Operation> operations, BufferedImage img) {
