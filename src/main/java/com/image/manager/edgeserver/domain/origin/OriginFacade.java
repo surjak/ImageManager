6af5f83e871cd8c1e4ce9146705a748242c9e892
Nazar Kordiumov <nazkord@gmail.com>
2021-04-22 00:00:07 +0200

    add metrics (#27) (#28)

--- src/main/java/com/image/manager/edgeserver/domain/origin/OriginFacade.java
+++ src/main/java/com/image/manager/edgeserver/domain/origin/OriginFacade.java
@@ -3,6 +3,8 @@ package com.image.manager.edgeserver.domain.origin;
 import com.image.manager.edgeserver.application.config.cache.RocksDBRepository;
 import com.image.manager.edgeserver.common.converter.BufferedImageConverter;
 import com.image.manager.edgeserver.domain.operation.Operation;
+import io.micrometer.core.instrument.DistributionSummary;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
@@ -24,24 +26,35 @@ import java.util.stream.Collectors;
 public class OriginFacade {
 
     private final Map<String, Origin> origins;
-
     private final BufferedImageConverter imageConverter;
-
     private final RocksDBRepository rocksDBRepository;
+    private final DistributionSummary originInboundTraffic;
+    private final DistributionSummary originOutboundTraffic;
 
     public OriginFacade(
             RocksDBRepository rocksDBRepository,
             BufferedImageConverter imageConverter,
-            List<Origin> origins) {
+            List<Origin> origins,
+            PrometheusMeterRegistry mr) {
         this.rocksDBRepository = rocksDBRepository;
         this.imageConverter = imageConverter;
         this.origins = origins.stream().collect(Collectors.toMap(Origin::getHost, o -> o));
+        this.originInboundTraffic = DistributionSummary
+                .builder("origin.inbound.traffic.size")
+                .baseUnit("bytes")
+                .register(mr);
+        this.originOutboundTraffic = DistributionSummary
+                .builder("edge.outbound.traffic.size")
+                .baseUnit("bytes")
+                .register(mr);
     }
 
     public Mono<byte[]> getImageAndApplyOperations(String host, String fileName, List<Operation> operations) {
-
         return fetchImageFromOrigin(host, fileName)
-                .map(imageConverter::byteArrayToBufferedImage)
+                .map(imgBytes -> {
+                    originOutboundTraffic.record(imgBytes.length);
+                    return imageConverter.byteArrayToBufferedImage(imgBytes);
+                })
                 .flatMap(img -> applyOperationsOnImage(operations, img)).map(imageConverter::bufferedImageToByteArray);
     }
 
@@ -49,7 +62,10 @@ public class OriginFacade {
     public Mono<byte[]> fetchImageFromOrigin(String host, String imageName) {
         return rocksDBRepository.find(imageName).map(Mono::just).orElseGet(() -> Optional.ofNullable(this.origins.get(host))
                 .map(origin -> origin.fetchImageFromOrigin(imageName))
-                .map(result -> result.doOnSuccess(b -> rocksDBRepository.save(imageName, b)))
+                .map(result -> result.doOnSuccess(imgBytes -> {
+                    originInboundTraffic.record(imgBytes.length);
+                    rocksDBRepository.save(imageName, imgBytes);
+                }))
                 .orElse(Mono.error(new UnknownHostException("Origin host not found"))));
     }
 
