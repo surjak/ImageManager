13d240166e424604090d425fec76f30a42779702
Mateusz Surjak <surjak1999@gmail.com>
2021-05-06 18:15:55 +0200

    SCH-136 validation done

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,4 @@ ARG env=abc
 ENV env_arg=${env}
 RUN chmod +x mvnw
 RUN ./mvnw clean package
-ENTRYPOINT ["java", "-Dspring.profiles.active=${env_arg}", "-jar", "target/app.jar"]
+ENTRYPOINT ["java", "-Dspring.profiles.active=${env_arg}", "-jar", "-Xms700M", " -Xmx700M", "target/app.jar"]
--- src/main/java/com/image/manager/edgeserver/EdgeServerApplication.java
+++ src/main/java/com/image/manager/edgeserver/EdgeServerApplication.java
@@ -5,11 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.blockhound.BlockHound;
 import reactor.core.scheduler.ReactorBlockHoundIntegration;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+
 @SpringBootApplication
 public class EdgeServerApplication {
 
     public static void main(String[] args) {
 //        BlockHound.install();
+        int mb = 1024 * 1024;
+        MemoryMXBean memoryBean = ManagementFactory.getMemoryMXBean();
+        long xmx = memoryBean.getHeapMemoryUsage().getMax() / mb;
+        long xms = memoryBean.getHeapMemoryUsage().getInit() / mb;
+        System.out.println("Initial Memory (xms) : "+xms+"mb");
+        System.out.println("Max Memory (xmx) : "+xmx+"mb");
         SpringApplication.run(EdgeServerApplication.class, args);
     }
 
