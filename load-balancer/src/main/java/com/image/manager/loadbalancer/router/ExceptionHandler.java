054e5519fcb2b9e27f1e056a71504cae6b7ee208
Mateusz Surjak <surjak1999@gmail.com>
2021-06-06 15:45:39 +0200

    possible fix

---
+++ load-balancer/src/main/java/com/image/manager/loadbalancer/router/ExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.image.manager.loadbalancer.router;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebExceptionHandler;
+import reactor.core.publisher.Mono;
+
+/**
+ * Created by surjak on 06.06.2021
+ */
+@Component
+@Order(-2)
+public class ExceptionHandler implements WebExceptionHandler {
+
+    @Override
+    public Mono<Void> handle(ServerWebExchange serverWebExchange, Throwable throwable) {
+
+        if (throwable instanceof Exception) {
+            serverWebExchange.getResponse().setStatusCode(HttpStatus.BAD_REQUEST);
+            return serverWebExchange.getResponse().setComplete();
+        }
+        return Mono.error(throwable);
+    }
+}
